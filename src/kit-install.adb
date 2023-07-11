bc0e091ee8b99f4cfa02b9067f176d7c9ba83f55
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2023-07-11 07:15:49 +0200

    Update kit-install.adb
    Look for kit config in $HOME/.config/kit or $USERPROFILE/.config/kit

--- src/kit-install.adb
+++ src/kit-install.adb
@@ -8,6 +8,12 @@ package body Kit.Install is
 
    procedure Check_Library_Path;
 
+   function User_Home return String;
+
+   ------------------------
+   -- Check_Library_Path --
+   ------------------------
+
    procedure Check_Library_Path is
       use Ada.Strings.Unbounded;
 
@@ -37,6 +43,18 @@ package body Kit.Install is
 
       declare
          Path : constant String :=
+                  Ada.Directories.Compose
+                    (Ada.Directories.Compose
+                       (User_Home, ".config"),
+                     "kit");
+      begin
+         if Try (Path) then
+            return;
+         end if;
+      end;
+
+      declare
+         Path : constant String :=
            Ada.Environment_Variables.Value ("KIT_LIBRARY_PATH", "");
       begin
          if Try (Path) then
@@ -59,5 +77,22 @@ package body Kit.Install is
       Check_Library_Path;
       return To_String (Kit_Library_Path);
    end Library_Path;
+
+   function User_Home return String is
+      User_Profile : constant String :=
+                       Ada.Environment_Variables.Value
+                         ("USERPROFILE", "");
+      Home         : constant String :=
+                       Ada.Environment_Variables.Value
+                         ("HOME", "");
+   begin
+      if User_Profile /= "" then
+         return User_Profile;
+      elsif Home /= "" then
+         return Home;
+      else
+         return "";
+      end if;
+   end User_Home;
 
 end Kit.Install;
